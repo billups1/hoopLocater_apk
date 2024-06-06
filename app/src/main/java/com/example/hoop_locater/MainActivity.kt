0c06274470e58d9bd1e139c5a3089f8bab09fb18
billups1 <eglesv2@hanmail.net>
2024-06-06 10:33:18 +0900

    내 위치 가져오기

--- app/src/main/java/com/example/hoop_locater/MainActivity.kt
+++ app/src/main/java/com/example/hoop_locater/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.hoop_locater
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.hoop_locater.databinding.ActivityMainBinding
 import com.example.hoop_locater.dto.hoop.Hoop
 import com.example.hoop_locater.dto.hoop.HoopList
@@ -26,41 +29,38 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+    val PERM_PLAG = 99
+
     lateinit var binding: ActivityMainBinding
 
     private lateinit var mapView: MapView
     private lateinit var googleMap: GoogleMap
-    private var currentMarker: Marker? = null
-    private var currentMarker2: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val retrofit = Retrofit.Builder().baseUrl("http://localhost:5000/")
-            .addConverterFactory(GsonConverterFactory.create()).build()
-        val service = retrofit.create(RetrofitService::class.java)
-
-        service.getHoopList()?.enqueue(object : Callback<HoopList>{
-            override fun onResponse(call: Call<HoopList>, response: Response<HoopList>) {
-                var result: HoopList? = response.body()
-                Log.d("YMC", "onResponse 성공: " + result?.toString());
-            }
-
-            override fun onFailure(call: Call<HoopList>, t: Throwable) {
-                Log.d("YMC", "onResponse 실패")
-            }
-        })
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        if (isPermitted()) {
+//            // 현재 위치로 카메라 이동
+//
+//        } else {
+//            ActivityCompat.requestPermissions(this, permissions, PERM_PLAG)
+//            Toast.makeText(this,"권한 승인이 필요합니다.",Toast.LENGTH_LONG).show()
+//        }
 
         this.mapView = binding.mapView
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
 
 
-        binding.navigationView.setOnItemSelectedListener{ item ->
+        binding.navigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.settingFragment -> {
                     Log.d("BottomNavigationView", "zxczxc")
@@ -76,17 +76,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
                 else -> {
                     return@setOnItemSelectedListener false
-                }                }
+                }
             }
-
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
 
         googleMap.setOnInfoWindowClickListener(object : OnInfoWindowClickListener {
             override fun onInfoWindowClick(marker: Marker) {
-                Toast.makeText(baseContext, "윈도우 클릭", Toast.LENGTH_SHORT).show()
-                Log.d("markerClick", "markerClick"+marker.title)
+                Log.d("markerClick", "markerClick" + marker.title)
 
                 val intent = Intent(this@MainActivity, PopupActivity::class.java)
                 intent.putExtra("id", marker.title)
@@ -99,43 +98,68 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 val intent = Intent(this@MainActivity, HoopCreateActivity::class.java)
                 intent.putExtra("latitude", latLng.latitude)
                 intent.putExtra("longitude", latLng.longitude)
+                Log.d("longClick", latLng.toString())
                 startActivity(intent)
             }
         })
 
+//         확대 축소 버튼
+        googleMap.uiSettings.isZoomControlsEnabled = true
+
+        // 내 위치 가져오기
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissions,
+                PERM_PLAG
+            )
+            return
+        }
+        googleMap.isMyLocationEnabled = true
+
+        val retrofit = Retrofit.Builder().baseUrl("http://10.0.2.2:5000/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        val service = retrofit.create(RetrofitService::class.java)
+
+        service.getHoopList()?.enqueue(object : Callback<HoopList> {
+            override fun onResponse(call: Call<HoopList>, response: Response<HoopList>) {
+                response.body()!!.forEach {
+                    setupMarker(it)
+                }
+            }
+
+            override fun onFailure(call: Call<HoopList>, t: Throwable) {
+                Log.d("YMC", "onResponse 실패" + t.toString() + "/" + t.message)
+            }
+        })
+
+        googleMap.moveCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(37.60794, 127.0090),
+                15f
+            )
+        )  // 카메라 이동
+
         this.googleMap = googleMap
-
-        val SEOUL = LatLng(37.556, 126.97)
-
-        currentMarker = setupMarker(LatLngEntity(37.5562,126.9724), "1111")  // default 서울역
-        currentMarker?.showInfoWindow()
-
-        currentMarker2 = setupMarker(LatLngEntity(37.5565,126.9727), "22222")
-        currentMarker2?.showInfoWindow()
-
-
-
     }
 
-    private fun setupMarker(locationLatLngEntity: LatLngEntity, sss : String): Marker? {
+    private fun setupMarker(hoop: Hoop) {
 
-        val positionLatLng = LatLng(locationLatLngEntity.latitude!!,locationLatLngEntity.longitude!!)
         val markerOption = MarkerOptions().apply {
-            position(positionLatLng)
-            title(sss)
-            snippet("서울역 위치")
+            position(LatLng(hoop.latitude, hoop.longitude))
+            title(hoop.name)
+            snippet("이 지역의 농구장")
         }
 
-        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL  // 지도 유형 설정
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(positionLatLng, 15f))  // 카메라 이동
-        return googleMap.addMarker(markerOption)
+        googleMap.addMarker(markerOption)?.showInfoWindow()
 
     }
-
-    data class LatLngEntity(
-        var latitude: Double?,
-        var longitude: Double?
-    )
-
 
 }
--- app/src/main/java/com/example/hoop_locater/RetrofitService.kt
+++ app/src/main/java/com/example/hoop_locater/RetrofitService.kt
@@ -1,11 +1,12 @@
 package com.example.hoop_locater
 
+import com.example.hoop_locater.dto.hoop.HoopList
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface RetrofitService {
 
-    @GET("api/v1/hoop/list")
-    fun getHoopList(): Call<Hoop>
+    @GET("api/v1/hoop/list/all")
+    fun getHoopList(): Call<HoopList>
 
 }
