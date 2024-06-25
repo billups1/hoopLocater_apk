10c24356b2b8d98dc1b93ebc9ec47e8434db0577
billups1 <eglesv2@hanmail.net>
2024-06-25 20:29:54 +0900

    설정 - 개인정보 처리방침 추가

--- app/src/main/java/com/real/hoop_locater/activity/SettingActivity.kt
+++ app/src/main/java/com/real/hoop_locater/activity/SettingActivity.kt
@@ -1,35 +1,34 @@
 package com.real.hoop_locater.activity
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences.Editor
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.real.hoop_locater.R
 import com.real.hoop_locater.adapter.RVAdapter
+import com.real.hoop_locater.databinding.ActivitySettingBinding
 import com.real.hoop_locater.menu.SettingMenu
 
 class SettingActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivitySettingBinding
     private val menuList = mutableListOf<SettingMenu>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_setting)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         menuList.add(SettingMenu.SET_DEFAULT_LOCATION)
+        menuList.add(SettingMenu.PRIVACY_POLICY)
 
         val recyclerView = findViewById<RecyclerView>(R.id.rv)
         val adapter = RVAdapter(baseContext, menuList)
@@ -41,6 +40,9 @@ class SettingActivity : AppCompatActivity() {
                     SettingMenu.SET_DEFAULT_LOCATION -> {
                         intent = Intent(baseContext, SetDefaultLocationActivity::class.java)
                     }
+                    SettingMenu.PRIVACY_POLICY -> {
+                        intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://billups1.github.io/"))
+                    }
                 }
                 startActivity(intent)
             }
@@ -48,6 +50,30 @@ class SettingActivity : AppCompatActivity() {
 
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        binding.navigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.settingFragment -> {
+                    startActivity(Intent(this, SettingActivity::class.java))
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.helpFragment -> {
+                    startActivity(Intent(this, HelpPopupActivity::class.java))
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.homeFragment -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> {
+                    return@setOnItemSelectedListener false
+                }
+            }
+        }
 
 
     }
--- app/src/main/java/com/real/hoop_locater/menu/SettingMenu.kt
+++ app/src/main/java/com/real/hoop_locater/menu/SettingMenu.kt
@@ -1,5 +1,6 @@
 package com.real.hoop_locater.menu
 
 enum class SettingMenu(val krName: String, val order: Int) {
-    SET_DEFAULT_LOCATION("앱 시작 시 지도 위치", 0)
+    SET_DEFAULT_LOCATION("앱 시작 시 지도 위치", 0),
+    PRIVACY_POLICY("개인정보 처리방침", 1)
 }
